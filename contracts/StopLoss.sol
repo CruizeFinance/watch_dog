330f2edb851e4646fcc3631e0329e52b9b26f0d1
Prithviraj8 <prithvimurthy8@gmail.com>
2021-11-01 13:41:51 +0530

    Add smart contract information

--- contracts/StopLoss.sol
+++ contracts/StopLoss.sol
@@ -2,6 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+@title "This contract swaps a given asset with a stablecoin if the users asset value is <= user specified dip_amount"
+@author "Prithviraj Murthy"
+@dev "This smart contract calls the chainlink contract priceFeed to get the latest price of the users asset and based on dip_amount, decides whether or not to swap it"
+@notices "This smart contract accepts a users asset information and calls the chainlink contract priceFeed and based on the user specified dip_amount, decides whether or not to swap it"
+@parameter "user_id, asset_address, total_asset_value, dip_amount"
+@return "A confirmation of whether or not the users asset has been successfully swapped with a stablecoin."
+
 contract StopLoss {
     uint public assetInformationCount = 0; //State variable written to the smart contract
 
