7dd52d443dd505ef4c0ba9ab9d81ac366bb5a5ba
AkhileshGoswami <agiri5375@gmail.com>
2021-11-11 20:27:47 +0530

    test2

--- test/StopLoss.test.js
+++ test/StopLoss.test.js
@@ -20,7 +20,8 @@ contract('StopLoss', (accounts) => {
         // console.log('feedRegistryInterfaceABI',feedRegistryInterfaceABI)
         // this.stopLoss = await StopLoss.new(feedRegistryInterfaceABI, 1, addr)
     })
-let test1 =1;
+let test2 =1;
+let test3 = 4;
     it('deploys successfully', async () =>{
         const address =await this.stopLoss.address
         console.log('address::', address)
